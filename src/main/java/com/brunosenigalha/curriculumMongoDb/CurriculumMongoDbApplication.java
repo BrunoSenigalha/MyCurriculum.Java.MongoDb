e4d1144e6e559e50616b1dfe5adafb47f83a2b83
Bruno Senigalha <brsenigalha@gmail.com>
2024-09-12 14:18:23 -0300

    first commit

---
+++ src/main/java/com/brunosenigalha/curriculumMongoDb/CurriculumMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.brunosenigalha.curriculumMongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunosenigalha/curriculumMongoDb/CurriculumMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosenigalha.curriculumMongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
