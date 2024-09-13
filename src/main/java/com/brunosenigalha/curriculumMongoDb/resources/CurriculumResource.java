5e0d8a0edad7dae7f0ae3c2dbed8f2eba78ac43b
Bruno Senigalha <brsenigalha@gmail.com>
2024-09-13 15:36:12 -0300

    Curriculum CRUD

--- src/main/java/com/brunosenigalha/curriculumMongoDb/resources/CurriculumResource.java
+++ src/main/java/com/brunosenigalha/curriculumMongoDb/resources/CurriculumResource.java
@@ -3,11 +3,12 @@ package com.brunosenigalha.curriculumMongoDb.resources;
 import com.brunosenigalha.curriculumMongoDb.entities.Curriculum;
 import com.brunosenigalha.curriculumMongoDb.services.CurriculumService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +22,35 @@ public class CurriculumResource {
     public ResponseEntity<List<Curriculum>> findAll() {
         List<Curriculum> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Curriculum> findById(@PathVariable String id) {
+        Curriculum obj = service.findById(id);
+        return ResponseEntity.ok(obj);
+    }
+    //    @PostMapping
+//    public ResponseEntity<Curriculum> insert(@RequestBody Curriculum obj){
+//        obj = service.insert(obj);
+//        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+//                .buildAndExpand(obj.getId()).toUri();
+//        return ResponseEntity.created(uri).body(obj);
+//    }
+    @PostMapping
+    public ResponseEntity<Curriculum> insert(@RequestBody Curriculum obj) {
+        obj = service.insert(obj);
+        return ResponseEntity.status(HttpStatus.CREATED).body(obj);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Curriculum> update(@PathVariable String id, @RequestBody Curriculum obj){
+        Curriculum entity = service.update(id, obj);
+        return ResponseEntity.ok(entity);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/brunosenigalha/curriculumMongoDb/services/CurriculumService.java
+++ src/main/java/com/brunosenigalha/curriculumMongoDb/services/CurriculumService.java
@@ -2,7 +2,10 @@ package com.brunosenigalha.curriculumMongoDb.services;
 
 import com.brunosenigalha.curriculumMongoDb.entities.Curriculum;
 import com.brunosenigalha.curriculumMongoDb.repositories.CurriculumRepository;
+import com.brunosenigalha.curriculumMongoDb.services.exceptions.DatabaseException;
+import com.brunosenigalha.curriculumMongoDb.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,5 +18,44 @@ public class CurriculumService {
 
     public List<Curriculum> findAll() {
         return repository.findAll();
+    }
+
+    public Curriculum findById(String id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Curriculum insert(Curriculum obj) {
+        return repository.save(obj);
+    }
+
+    public Curriculum update(String id, Curriculum obj){
+        return repository.findById(id)
+                .map(entity -> {
+                    updateData(entity, obj);
+                    return repository.save(entity);
+                })
+                .orElseThrow(()-> new ResourceNotFoundException(id));
+    }
+
+    public void delete(String id){
+        Curriculum entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+        try{
+            repository.delete(entity);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    private void updateData(Curriculum entity, Curriculum obj){
+        entity.setPicture(obj.getPicture());
+        entity.setName(obj.getName());
+        entity.setGender(obj.getGender());
+        entity.setAddressDTO(obj.getAddressDTO());
+        entity.setProfessionalGoals(obj.getProfessionalGoals());
+        entity.setPhone(obj.getPhone());
+        entity.setEmail(obj.getEmail());
+        entity.setLinkedIn(obj.getLinkedIn());
     }
 }
