5cc289c02e24e3eca2b555cb31d530f5f3fb019f
Bruno Senigalha <brsenigalha@gmail.com>
2024-09-13 15:48:14 -0300

    Course Crud

--- src/main/java/com/brunosenigalha/curriculumMongoDb/resources/CourseResource.java
+++ src/main/java/com/brunosenigalha/curriculumMongoDb/resources/CourseResource.java
@@ -1,12 +1,12 @@
 package com.brunosenigalha.curriculumMongoDb.resources;
 
 import com.brunosenigalha.curriculumMongoDb.entities.Course;
+import com.brunosenigalha.curriculumMongoDb.entities.Curriculum;
 import com.brunosenigalha.curriculumMongoDb.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,29 @@ public class CourseResource {
     public ResponseEntity<List<Course>> findAll() {
         List<Course> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Course> findById(@PathVariable String id) {
+        Course obj = service.findById(id);
+        return ResponseEntity.ok(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> insert(@RequestBody Course obj) {
+        obj = service.insert(obj);
+        return ResponseEntity.status(HttpStatus.CREATED).body(obj);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Course> update(@PathVariable String id, @RequestBody Course obj){
+        Course entity = service.update(id, obj);
+        return ResponseEntity.ok(entity);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/brunosenigalha/curriculumMongoDb/services/CourseService.java
+++ src/main/java/com/brunosenigalha/curriculumMongoDb/services/CourseService.java
@@ -1,8 +1,12 @@
 package com.brunosenigalha.curriculumMongoDb.services;
 
 import com.brunosenigalha.curriculumMongoDb.entities.Course;
+import com.brunosenigalha.curriculumMongoDb.entities.Curriculum;
 import com.brunosenigalha.curriculumMongoDb.repositories.CourseRepository;
+import com.brunosenigalha.curriculumMongoDb.services.exceptions.DatabaseException;
+import com.brunosenigalha.curriculumMongoDb.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,5 +19,39 @@ public class CourseService {
 
     public List<Course> findAll() {
         return repository.findAll();
+    }
+
+    public Course findById(String id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Course insert(Course obj) {
+        return repository.save(obj);
+    }
+
+    public Course update(String id, Course obj){
+        return repository.findById(id)
+                .map(entity -> {
+                    updateData(entity, obj);
+                    return repository.save(entity);
+                })
+                .orElseThrow(()-> new ResourceNotFoundException(id));
+    }
+
+    public void delete(String id){
+        Course entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+        try{
+            repository.delete(entity);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    private void updateData(Course entity, Course obj){
+        entity.setTypeCourse(obj.getTypeCourse());
+        entity.setTitle(obj.getTitle());
+        entity.setDescription(obj.getDescription());
     }
 }
